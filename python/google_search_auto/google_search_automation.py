0ce47c709d6efcb576b5212036999ca23749c938
marklasia <mark.f.lasia@gmail.com>
2019-11-04 11:07:36 +0100

    commit saved changes

--- python/google_search_auto/google_search_automation.py
+++ python/google_search_auto/google_search_automation.py
@@ -3,12 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 from sys import argv
 
-class Analysis:
+class SearchGoogle:
     def __init__(self, term):
         self.term = term
-        self.subjectivity = 0
-        self.sentiment = 0
-
         self.url = 'https://www.google.com/search?q={0}&source=lnms&tbm=nws'.format(self.term)
 
     def run (self):
@@ -22,7 +19,7 @@ class Analysis:
                 
 
 
-a = Analysis('bitcoin')
+a = SearchGoogle('boeing')
 a.run()
 
 print(a.term)
